7767deb931c92183b859b0d1b72209b3c9755241
Lauri Liuhto <lliuhto@cs.helsinki.fi>
2004-09-21 12:49:21 +0000

    Titokoneen sorsat cvs:ään - toivottavasti menee oikein, oli vähän ongelmia / Lauri

---
+++ titokone/fi/hu/cs/ttk91/TTK91CompileSource.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Feb 24, 2004
+ */
+package fi.hu.cs.ttk91;
+
+/*
+ * See separate documentation in yhteisapi.pdf in the javadoc root.
+ */
+public interface TTK91CompileSource {
+	
+	/**
+	 * 
+	 * @return the source code
+	 */
+	public String getSource();
+}
---
+++ titokone/fi/hu/cs/ttk91/TTK91Memory.java
@@ -0,0 +1,29 @@
+/*
+ * Created on Feb 24, 2004
+ */
+package fi.hu.cs.ttk91;
+
+import java.util.HashMap;
+
+/*
+ * See separate documentation in yhteisapi.pdf in the javadoc root.
+ */
+public interface TTK91Memory {
+	public int getSize();
+	/**
+	 * First usable index is 0, last is getSize()-1
+	 * @param memoryslot memory address where required data is 
+	 * @return data from requested address
+	 */
+	public int getValue(int memoryslot);
+	
+	/**
+	 * 
+	 * @return HashMap with symbol name as a key, and Integer as value. 
+	 *         Integer describes the memory slot where the value is stored.
+	 */
+	public HashMap getSymbolTable();
+	public int[] getMemory();
+	public int[] getCodeArea();
+	public int[] getDataArea(); 
+}
