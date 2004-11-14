6b4dcf02a2356f3d1ca99e6a2774df4228adcf8c
Harri Tuomikoski <htuomiko@cs.helsinki.fi>
2004-11-14 15:06:10 +0000

    *** empty log message ***

---
+++ fi/helsinki/cs/koskelo/common/TTK91TaskFeedback.java
@@ -0,0 +1,183 @@
+package fi.helsinki.cs.koskelo.common;
+
+public class TTK91TaskFeedback {
+
+ public void setMaxSizeFeedbackPositive(String positive) {
+
+ }//setMaxSizeFeedback
+
+ public void setMaxSizeFeedbackNegative(String negative) {
+
+ }//setMaxSizeFeedbackNegative
+
+ Asettaa positiivisen palautteen ohjelman optimaalisesta koosta.
+ public void setOptimalSizeFeedbackPositive(String positive) {
+
+ }//setOptimalSizeFeedbackPositive
+
+ Asettaa negatiivisen palautteen ohjelman optimaalisesta koosta.
+ public void setOptimalSizeFeedbackNegative(String negative) {
+
+ }//setOptimalSizeFeedbackNegative
+
+ Asettaa positiivisen palautteen ohjelman vaadituista käskyistä.
+ public void setRequiredCommandsFeedbackPositive(String positive) {
+
+ }//setRequiredCommandsFeedbackPositive
+
+ Asettaa negatiivisen palautteen ohjelman vaadituista käskyistä.
+ public void setRequiredCommandsFeedbackNegative(String negative) {
+
+ }//setRequiredCommandsFeedbackNegative
+
+ Asettaa positiivisen palautteen ohjelman kielletyistä käskyistä.
+ public void setForbiddenCommandsFeedbackPositive(String positive) {
+
+ }//setForbiddenCommandsFeedbackPositive
+
+ Asettaa negatiivisen palautteen ohjelman kielletyistä käskyistä.
+ public void setForbiddenCommandsFeedbackNegative(String negative) {
+
+ }//setForbiddenCommandsFeedbackNegative
+
+ Asettaa positiivisen palautteen rekistereistä.
+ public void setRegisterFeedbackPositive(String positive) {
+
+ }//setRegisterFeedbackPositive
+
+ Asettaa negatiivisen palautteen rekistereistä.
+ public void setRegisterFeedbackNegative(String negative) {
+
+ }//setRegisterFeedbackNegative
+
+ Asettaa positiivisen palautteen muistipaikoista ja muuttujista.
+ public void setMemoryFeedbackPositive(String positive) {
+
+ }//setMemoryFeedbackPositive
+
+ Asettaa negatiivisen palautteen muistipaikoista ja muuttujista.
+ public void setMemoryFeedbackNegative(String negative) {
+
+ }//setMemoryFeedbackNegative
+
+ Asettaa positiivisen palautteen muistiviitteiden määrästä.
+ public void setMemoryReferencesFeedbackPositive(String positive) {
+
+ }//setMemoryReferencesFeedbackPositive
+
+ Asettaa negatiivisen palautteen muistiviitteiden määrästä.
+ public void setMemoryReferencesFeedbackNegative(String negative) {
+
+ }//setMemoryReferencesFeedbackNegative
+
+ Asettaa positiivisen palautteen näytön tulosteista.
+ public void setScreenOutputFeedbackPositive(String positive) {
+
+ }//setScreenOutputFeedbackPositive
+
+ Asettaa negatiivisen palautteen näytön tulosteista.
+ public void setScreenOutputFeedbackNegative(String negative) {
+
+ }//setScreenOutputFeedbackNegative
+
+ Asettaa positiivisen palautteen tiedoston tulosteista.
+ public void setFileOutputFeedbackPositive(String positive) {
+
+ }//setFileOutputFeedbackPositive
+
+ Asettaa negatiivisen palautteen tiedoston tulosteista.
+ public void setFileOutputFeedbackNegative(String negative) {
+
+ }//setFileOutputFeedbackNegative
+
+ Antaa positiivisen palautteen maksimikoosta.
+ public String getMaxSizeFeedbackPositive() {
+
+ }//getMaxSizeFeedbackPositive
+
+ Antaa negatiivisen palautteen maksimikoosta.
+ public String getMaxSizeFeedbackNegative() {
+
+ }//getMaxSizeFeedbackNegative
+
+ Antaa positiivisen palautteen optimaalisesta koosta.
+ public String getOptimalSizeFeedbackPositive() {
+
+ }//getOptimalSizeFeedbackPositive
+
+ Antaa negatiivisen palautteen optimaalisesta koosta.
+ public String getOptimalSizeFeedbackNegative() {
+
+ }//getOptimalSizeFeedbackNegative
+
+ Antaa positiivisen palautteen vaadituista käskyistä.
+ public String getRequiredCommandsFeedbackPositive() {
+
+ }//getRequiredCommandsFeedbackPositive
+
+ Antaa negatiivisen palautteen vaadituista käskyistä.
+ public String getRequiredCommandsFeedbackNegative() {
+
+ }//getRequiredCommandsFeedbackNegative
+
+ Antaa positiivisen palautteen kielletyistä käskyistä.
+ public String getForbiddenCommandsFeedbackPositive() {
+
+ }//getForbiddenCommandsFeedbackPositive
+
+ Antaa negatiivisen palautteen kielletyistä käskyistä.
+ public String getForbiddenCommandsFeedbackNegative() {
+
+ }//getForbiddenCommandsFeedbackNegative
+
+ Antaa positiivisen palautteen rekistereistä.
+ public String getRegisterFeedbackPositive() {
+
+ }//getRegisterFeedbackPositive
+
+ Antaa negatiivisen palautteen rekistereistä.
+ public String getRegisterFeedbackNegative() {
+
+ }//getRegisterFeedbackNegative
+
+ Antaa positiivisen palautteen muistipaikoista ja muuttujista.
+ public String getMemoryFeedbackPositive() {
+
+ }//getMemoryFeedbackPositive
+
+ Antaa negatiivisen palautteen muistipaikoista ja muuttujista.
+ public String getMemoryFeedbackNegative() {
+
+ }//getMemoryFeedbackNegative
+
+ Antaa positiivisen palautteen muistiviitteiden määrästä.
+ public String getMemoryReferencesFeedbackPositive() {
+
+ }//getMemoryReferencesFeedbackPositive
+
+ Antaa negatiivisen palautteen muistiviitteiden määrästä.
+ public String getMemoryReferencesFeedbackNegative() {
+
+ }//getMemoryReferencesFeedbackNegative
+
+ Antaa positiivisen palautteen näytön tulosteista.
+ public String getScreenOutputFeedbackPositive() {
+
+ }//getScreenOutputFeedbackPositive
+
+ Antaa negatiivisen palautteen näytön tulosteista.
+ public String getScreenOutputFeedbackNegative() {
+
+ }//getScreenOutputFeedbackNegative
+
+ Antaa positiivisen palautteen tiedoston tulosteista.
+ public String getFileOutputFeedbackPositive() {
+
+ }//getFileOutputFeedbackPositive
+
+ Antaa negatiivisen palautteen tiedoston tulosteista.
+ public String getFileOutputFeedbackNegative() {
+
+ }//getFileOutputFeedbackNegative
+
+}//class
