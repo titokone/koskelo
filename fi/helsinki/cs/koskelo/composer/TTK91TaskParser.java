3eedd4d44175f65547bc9afd80daefdd72c0cd0f
Eeva Nevalainen <emnevala@cs.helsinki.fi>
2004-11-17 09:25:37 +0000

    Vhänä.

--- fi/helsinki/cs/koskelo/composer/TTK91TaskParser.java
+++ fi/helsinki/cs/koskelo/composer/TTK91TaskParser.java
@@ -1,5 +1,8 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
+import fi.helsinki.cs.koskelo.common.*;
+
+package fi.helsinki.cs.koskelo.composer;
 
 public class TTK91TaskParser {
 	/*
@@ -61,22 +64,22 @@ public class TTK91TaskParser {
 	}
 
 	private void assembleCommon() {
-		newTask.set("acceptedSize", tops.getAcceptedSize);
-		newTask.set("compareMethod", tops.getCompareMethod);
+		newTask.set("acceptedSize", tops.getAcceptedSize());
+		newTask.set("compareMethod", tops.getCompareMethod());
 		newTask.set("exampleCode", tops.getExampleCode);
-		newTask.set("fileOutputCriterias", tops.getFileOutputCriterias);
-		newTask.set("forbiddenCommands", tops.getForbiddenCommands);
-		newTask.set("hiddenInput", tops.getHiddenInput);
-		newTask.set("maxCommands", tops.getMaxCommands);
-		newTask.set("memoryCriterias", tops.getMemoryCriterias);
-		newTask.set("memoryReferences", tops.getMemoryReferences); // ?
-		newTask.set("optimalSize", tops.getOptimalSize);
-		newTask.set("publicInput", tops.getPublicInput);
-		newTask.set("registerCriterias", tops.getRegisterCriterias);
-		newTask.set("requiredCommands", tops.getRequiredCommands);
+		newTask.set("fileOutputCriterias", tops.getFileOutputCriterias());
+		newTask.set("forbiddenCommands", tops.getForbiddenCommands());
+		newTask.set("hiddenInput", tops.getHiddenInput());
+		newTask.set("maxCommands", tops.getMaxCommands());
+		newTask.set("memoryCriterias", tops.getMemoryCriterias());
+		newTask.set("memoryReferences", tops.getMemoryReferences()); // ?
+		newTask.set("optimalSize", tops.getOptimalSize());
+		newTask.set("publicInput", tops.getPublicInput());
+		newTask.set("registerCriterias", tops.getRegisterCriterias());
+		newTask.set("requiredCommands", tops.getRequiredCommands());
 		newTask.set("screenOutputCriterias",
-				tops.getScreenOutputCriterias);
-		newTask.set("taskDescription", tops.getTaskDescription);
+				tops.getScreenOutputCriterias());
+		newTask.set("taskDescription", tops.getTaskDescription());
 		newTask.set("taskFeedback", tops.getTaskFeedback); // ?
 
 		// ?:lliset  näyttävät puuttuvan TTK91TaskOptionsista...
