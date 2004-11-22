f669d6b2220cded0848b212c7f75f440a62b0055
Eeva Nevalainen <emnevala@cs.helsinki.fi>
2004-11-22 09:20:33 +0000

    Kirjoitettu "kattavat" testit

--- fi/helsinki/cs/koskelo/test/DisplayerUtilsTest.java
+++ fi/helsinki/cs/koskelo/test/DisplayerUtilsTest.java
@@ -15,11 +15,20 @@ public class DisplayerUtilsTest extends TestCase {
 
 	public void testgetHTMLElementInput() {
 
+	Assert.assertEquals(fi.helsinki.cs.koskelo.displayer.TTK91DisplayerUtils.getHTMLElementInput("Tehtävä 1", "testi 1"), "<p class=\"input\"><strong>Tehtävä 1: testi 1</strong></p>");
 	}
 
 
-	public void testHTMLElementAnswerBox() throws Exception{
+	public void testHTMLElementAnswerBox(){
+	Assert.assertEquals(
+			fi.helsinki.cs.koskelo.displayer.TTK91DisplayerUtils.getHTMLElementAnswerBox(3,4), 
+			"<textarea name=\"answer\" cols=\"4\" rows=\"3\"></textarea><br>"
+			);
+	String[] tmp = {"Foobar"};
+	Assert.assertEquals(
+			fi.helsinki.cs.koskelo.displayer.TTK91DisplayerUtils.getHTMLElementAnswerBox(tmp,3,4),
+			"<textarea name=\"answer\" cols=\"4\" rows=\"3\">Foobar</textarea><br>"
+			);
 
 	}
-
 }
