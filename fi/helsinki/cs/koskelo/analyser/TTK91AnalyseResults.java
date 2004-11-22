fc0a8d4add2376f4853d0a25cb218800bba8391b
Eeva Nevalainen <emnevala@cs.helsinki.fi>
2004-11-22 14:24:17 +0000

    Dummy lisätty

---
+++ fi/helsinki/cs/koskelo/analyser/TTK91AnalyseResults.java
@@ -0,0 +1,19 @@
+public class TTK91AnalyseResults{
+
+	private boolean X;
+
+	public TTK91AnalyseResults() {
+		X = false;
+
+	}
+
+
+	public void setX(boolean b) {
+		this.X = b;
+	}
+
+	public boolean getX() {
+		return this.X
+	}
+
+}
