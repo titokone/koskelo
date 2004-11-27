a075d4704132247cb8e7347273d6a745e8fe8acd
Lauri Liuhto <lliuhto@cs.helsinki.fi>
2004-11-27 14:57:53 +0000

    lisätty eAssarin virhenumerot / Lauri

--- fi/helsinki/cs/koskelo/common/TTK91Constant.java
+++ fi/helsinki/cs/koskelo/common/TTK91Constant.java
@@ -6,20 +6,25 @@ package fi.helsinki.cs.koskelo.common;
 
 public final class TTK91Constant {
 
-  // common.TTK91TaskOptions
-	public final static int COMPARE_TO_SIMULATED = 0; 
-	public final static int COMPARE_TO_STATIC = 1;
-  public final static int MAX_COMMANDS = 10000; 
+    // common.TTK91TaskOptions
+    public final static int COMPARE_TO_SIMULATED = 0; 
+    public final static int COMPARE_TO_STATIC = 1;
+    public final static int MAX_COMMANDS = 10000; 
 
-  // common.TTK91TaskCriteria
-  public static final int INVALID = -1; // Alustamaton vertailu.
-  public static final int LESS = 0; // <
-  public static final int LESSEQ = 1; // <=
-  public static final int GREATER = 2; // >
-  public static final int GREATEREQ = 3; // >=
-  public static final int EQUAL = 4; // =
-  public static final int NOTEQUAL = 5; // !=
-  public static final int NOTCOMPARABLE = 6; // Tulosteita varten joissa
-                                   // ei ole loogista operaattoria
+    // common.TTK91TaskCriteria
+    public static final int INVALID = -1; // Alustamaton vertailu.
+    public static final int LESS = 0; // <
+    public static final int LESSEQ = 1; // <=
+    public static final int GREATER = 2; // >
+    public static final int GREATEREQ = 3; // >=
+    public static final int EQUAL = 4; // =
+    public static final int NOTEQUAL = 5; // !=
+    public static final int NOTCOMPARABLE = 6; // Tulosteita varten joissa
+                                               // ei ole loogista operaattoria
+
+    // eAssarin virhekoodit
+    public static final int FATAL_ERROR = 2;
+    //    public static final int ERROR = 1; <-- tätä ei taideta käyttää missään, eikä taida olla varsinaisesti edes määritelty
+    public static final int NO_ERROR = 0;
 
 }
