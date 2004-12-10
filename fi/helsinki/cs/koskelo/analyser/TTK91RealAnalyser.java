a9f70aeb862e811efcaaba7ae3b83a60681c66fd
Lauri Liuhto <lliuhto@cs.helsinki.fi>
2004-12-10 21:01:36 +0000

    TTK91RealAnalyserin "stubi". Tästä se lähtee... / Lauri

---
+++ fi/helsinki/cs/koskelo/analyser/TTK91RealAnalyser.java
@@ -0,0 +1,11 @@
+package fi.helsinki.cs.koskelo.analyser;
+
+public class TTK91RealAnalyser {
+
+    public TTK91RealAnalyser(TTK91AnalyseData analyseData) {
+    }
+
+    public Feedback analyse() {
+	return new Feedback();
+    }
+}
