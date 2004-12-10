46e7839882ed355bd2ccd486a9f3ad4d1fb6e424
Lauri Liuhto <lliuhto@cs.helsinki.fi>
2004-12-10 22:19:11 +0000

    *** empty log message ***

--- fi/helsinki/cs/koskelo/analyser/TTK91RealAnalyser.java
+++ fi/helsinki/cs/koskelo/analyser/TTK91RealAnalyser.java
@@ -1,11 +1,14 @@
 package fi.helsinki.cs.koskelo.analyser;
 
+//import fi.helsinki.cs.koskelo.analyser
+
+
 public class TTK91RealAnalyser {
 
     public TTK91RealAnalyser(TTK91AnalyseData analyseData) {
     }
 
-    public Feedback analyse() {
-	return new Feedback();
+    public TTK91AnalyseResults analyse() {
+	return new TTK91AnalyseResults();
     }
 }
