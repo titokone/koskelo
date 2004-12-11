fef63ed24ed33ed36a500cf0ffcc58f2cb742642
Lauri Liuhto <lliuhto@cs.helsinki.fi>
2004-12-11 13:27:34 +0000

    *** empty log message ***

--- fi/helsinki/cs/koskelo/analyser/TTK91RealAnalyser.java
+++ fi/helsinki/cs/koskelo/analyser/TTK91RealAnalyser.java
@@ -1,14 +1,18 @@
 package fi.helsinki.cs.koskelo.analyser;
 
-//import fi.helsinki.cs.koskelo.analyser
-
-
 public class TTK91RealAnalyser {
 
     public TTK91RealAnalyser(TTK91AnalyseData analyseData) {
     }
 
     public TTK91AnalyseResults analyse() {
+	// analysoi seuraavat osiot
+	// -'generalAnalysis'
+	// -'analyseMemory'
+	// -'analyseRegisters'
+	// -'analyseOutput'
+	// - statistiikat
+
 	return new TTK91AnalyseResults();
     }
 }
